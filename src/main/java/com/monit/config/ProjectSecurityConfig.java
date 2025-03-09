ec469d50b65a47ffef3c2f35f9c84a268b411c42
monitranjan <monitranjan2050@gmail.com>
2025-03-09 21:46:30 +0530

    using jdbcUserDetailsManager for authentication with database

--- src/main/java/com/monit/config/ProjectSecurityConfig.java
+++ src/main/java/com/monit/config/ProjectSecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -38,17 +38,14 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}bcrypt@12345")
-                .authorities("read")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$g./CFwfixaPjcrXuN3nRDOCNmhbyDGfHLGzfgjzEc6F4z.Aa8CLam")
-                .authorities("admin")
-                .build();
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        /*
+        UserDetails user = User.withUsername("user").password("{noop}bcrypt@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$g./CFwfixaPjcrXuN3nRDOCNmhbyDGfHLGzfgjzEc6F4z.Aa8CLam").authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
+         */
+//        now converting with mysql
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
