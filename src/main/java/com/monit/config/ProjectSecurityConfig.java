9eff211c8c90971fe9401b3631032e98b051021d
monitranjan <monitranjan2050@gmail.com>
2025-03-09 17:19:29 +0530

    Adding inmemory users as via application we were only able to add single user without roles

--- src/main/java/com/monit/config/ProjectSecurityConfig.java
+++ src/main/java/com/monit/config/ProjectSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -30,4 +34,19 @@ public class ProjectSecurityConfig {
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}12345")
+                .authorities("read")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}54321")
+                .authorities("admin")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
