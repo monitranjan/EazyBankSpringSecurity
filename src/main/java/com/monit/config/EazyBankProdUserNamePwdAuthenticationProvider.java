eb441265f0042f48b43a490f70ad6f8dd736c52c
monitranjan <monitranjan2050@gmail.com>
2025-03-11 00:40:48 +0530

    Adding authentication based on environment profiles

---
+++ src/main/java/com/monit/config/EazyBankProdUserNamePwdAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package com.monit.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile(value = "prod")
+@RequiredArgsConstructor
+public class EazyBankProdUserNamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final EazyBankUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            //fetch age details and perform validation to check if age>18 or any custom needs
+            return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        } else {
+            throw new AuthenticationException("Invalid username or password") {
+            };
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
--- src/main/java/com/monit/config/EazyBankUserDetailsService.java
+++ src/main/java/com/monit/config/EazyBankUserDetailsService.java
@@ -3,6 +3,7 @@ package com.monit.config;
 import com.monit.model.Customer;
 import com.monit.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Profile("!prod")
 @RequiredArgsConstructor // for single field we don't need @Autowired also method injection will be handled by this annotation
 public class EazyBankUserDetailsService implements UserDetailsService {
 
