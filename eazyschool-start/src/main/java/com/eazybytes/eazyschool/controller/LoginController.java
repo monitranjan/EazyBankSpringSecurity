09a5ba1ea4c9b54ded2ae544fe16f6ba2ce67777
monitranjan <monitranjan2050@gmail.com>
2025-03-11 17:53:31 +0530

    form login with redirection chnages

--- eazyschool-start/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
+++ eazyschool-start/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
@@ -12,7 +12,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
     @RequestMapping(value = "/login", method = {RequestMethod.GET})
-    public String displayLoginPage() {
+    public String displayLoginPage(@RequestParam(value = "error", required = false) String error, Model model) {
+        String errorMessage = null;
+        if(null != error && error.equals("true")) {
+            errorMessage = "Invalid username or password";
+        }
+        model.addAttribute("errorMessage", errorMessage);
         return "login.html";
     }
 
